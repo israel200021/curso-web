cfebe37ddf58f130fe0f8e147e81d1fb919b9aaf
israel <isramr2000@gmail.com>
2023-03-31 00:35:27 -0600

    Convirtiendo archivos a php he incluyendo templates de header y footer

---
+++ bienesraices_inicio/base.php
@@ -0,0 +1,26 @@
+<?php
+    include './includes/templates/header.php';
+?>
+
+    <main class="contenedor seccion">
+        <h1>Nosotros</h1>
+
+
+    </main>
+
+    <footer class="footer seccion">
+        <div class="contenedor contenedor-footer">
+            <nav class="navegacion">
+                <a href="nosotros.html">Nosostros</a>
+                <a href="anuncios.html">Anuncios</a>
+                <a href="blog.html">Blog</a>
+                <a href="contacto.html">Contacto</a>
+             </nav>
+        </div>
+        <p class="copyright">Todos los derechos reservados 2021</p>
+    </footer>
+
+
+    <script src="build/js/bundle.min.js"></script>
+</body>
+</html>
